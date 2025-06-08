d0b5abff9c95cb3c843fc2f0d14bec46672262ae
aharizdanish <a.harizdanish@gmail.com>
2025-06-08 21:43:20 +0800

    MiniProject.py

--- MiniProject.py
+++ MiniProject.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
     
-st.title("""Student Performance Dashboard""")
+st.title("""Student Performan Dashboard""")
 
 
 df = pd.read_csv("Students.data.csv")
