cf3288090393d89330995f0877040ed88dbce6c1
aharizdanish <a.harizdanish@gmail.com>
2025-06-08 21:57:01 +0800

    MiniProject.py

--- MiniProject.py
+++ MiniProject.py
@@ -3,6 +3,23 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+page_bg_img = f"""
+<style>
+[data-testid="stAppViewContainer"] > .main {{
+background-image: url("https://i.postimg.cc/4xgNnkfX/Untitled-design.png");
+background-size: cover;
+background-position: center center;
+background-repeat: no-repeat;
+background-attachment: local;
+}}
+[data-testid="stHeader"] {{
+background: rgba(0,0,0,0);
+}}
+</style>
+"""
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
 st.image("banner.jpeg", caption=None)
 st.title("""Student Performance Dashboard""")
 
